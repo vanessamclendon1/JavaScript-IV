caca71fd36d3f46c8d745eac5c5186a9e9c9bba0
Vanessa McLendon <vanessamclendon@Vanessas-MacBook-Pro-2.local>
2019-02-28 15:53:33 -0500

    build student instructor and person constructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,61 @@
 // CODE here for your Lambda Classes
+
+//Base-class
+class Person{
+    constructor(people){
+        this.name = people.name;
+        this.age = people.age;
+        this.location = people.location;
+        this.gender = people.gender;
+    }
+    /**Method */
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor{
+    constructor(teacher){
+        this.specialty = teacher.specialty;
+        this.favLanguage = teacher.favLanguage;
+        this.catchPhrase = teacher.catchPhrase;
+    }
+    demo() {
+        return `Today we are learning about ${this.subject}.`;
+    }
+    grade(){
+        return `${student.name} received a perfect score on ${this.subject}.`
+    }
+}
+
+class Student{
+    constructor(partof){
+        this.previousBackground = partof.previousBackground;
+        this.className = partof.className;
+        this.favSubjects = favSubjects.className;
+    }
+    /**Methods */
+    listsSubjects(){
+        return `${favSubjects}`;
+    }
+    PRAssignment(){
+       return `${this.favSubjects} `;
+    }
+    sprintChallenge() {
+        //return `{this.}`
+    }
+}
+
+class Project_Manager{
+    
+}
+
+
+const fred = new Person({
+    name: 'Fred',
+    age: 'Bedrock',
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: 'Dont forget the homies'
+});
